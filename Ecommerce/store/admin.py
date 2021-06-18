8064e5703b7d245ddd23a3c050996d4e82c623fc
pysunita <sunitasahu.nl@gmail.com>
2021-06-18 20:25:55 +0200

    worked on cart page and checkout page

--- Ecommerce/store/admin.py
+++ Ecommerce/store/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from.models import *
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['user','name','email']
+admin.site.register(Customer,CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','price','image']
+admin.site.register(Product,ProductAdmin)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer','date_ordered','transaction_id']
+admin.site.register(Order,OrderAdmin)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['product','order','quantity', 'date_added']
+admin.site.register(OrderItem,OrderItemAdmin)
 admin.site.register(ShippingAddress)
--- Ecommerce/store/models.py
+++ Ecommerce/store/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 
 class Customer(models.Model):
-    user=models.OneToOneField(User, null=True,blank=True,on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True,blank=True)
     name=models.CharField(max_length=200,null=True)
     email=models.CharField(max_length=200,null=True)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -17,8 +17,16 @@ class Product(models.Model):
     digital=models.BooleanField(default=False, null=True,blank=True)
     image = models.ImageField(null=True, blank=True)
 
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer=models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True,blank=True )
@@ -26,16 +34,33 @@ class Order(models.Model):
     complete=models.BooleanField(default=False)
     transaction_id=models.CharField(max_length=100,null=True)
 
-def __str__(self):
-    return str(self.id)
+    def __str__(self):
+        return str(self.id)
+
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
 
 
 class OrderItem(models.Model):
     product=models.ForeignKey(Product, on_delete=models.SET_NULL,null=True,blank=True )
     order=models.ForeignKey(Order, on_delete=models.SET_NULL,null=True,blank=True )
-    quality=models.IntegerField(default=0,null=True,blank=True)
+    quantity=models.IntegerField(default=0,null=True,blank=True)
     date_added=models.DateTimeField(auto_now_add=True)
 
+    @property
+    def get_total(self):
+        total=self.product.price * self.quantity
+        return total
 class ShippingAddress(models.Model):
     customer=models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True)
     order=models.ForeignKey(Order, on_delete=models.SET_NULL,null=True)
